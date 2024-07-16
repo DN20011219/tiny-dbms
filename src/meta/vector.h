3be435848f658fd1dc83b7a6131b70491efcb77a
Ding Ning <dingning@whu.edu.cn>
2024-07-16 16:32:51 +0800

    新增元数据类型-向量

---
+++ src/meta/vector.h
@@ -0,0 +1,53 @@
+// Copyright (c) 2024 by dingning
+//
+// file  : vector.h
+// since : 2024-07-16
+// desc  : basic vector struct, support different type of data and length (such as int, size_t, long, unsigned int)
+
+#ifndef VDBMS_META_VECTOR_H_
+#define VDBMS_META_VECTOR_H_
+
+#include <cassert>
+
+template<typename LENGTH_TYPE, typename DATA_TYPE>
+class Vector
+{
+private:
+    unsigned int data_size;    // the space cost of one data item
+    LENGTH_TYPE vector_length; // length of the vector
+    DATA_TYPE* data;           // store the data of the vector
+
+public:
+
+    /**
+    * @brief Init a empty vector using input length as space
+    * @param length Length of this vector.
+    */
+    Vector(LENGTH_TYPE length) {
+        data_size = sizeof(DATA_TYPE);
+        vector_length = length;
+        data = new DATA_TYPE[vector_length];
+    }
+
+    /**
+    * @brief Init a vector, fill the data using input data.
+    * @param data data to store.
+    * @param length length of data.
+    */
+    Vector(DATA_TYPE* data, LENGTH_TYPE length) {
+        data_size = sizeof(DATA_TYPE);
+        vector_length = length;
+        this->data = data;
+    } 
+    
+    inline DATA_TYPE Get(LENGTH_TYPE position) {
+        assert(position >= 0 && position < vector_length);
+        return *(data + position);
+    }
+
+    ~Vector() {
+        delete[] data;
+    }
+};
+
+#endif // VDBMS_META_VECTOR_H_
