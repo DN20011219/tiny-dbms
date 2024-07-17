1eb6799b3082ab9d625f14f70b41b030c40e5b5f
dingning14 <dingning14@meituan.com>
2024-07-17 18:20:41 +0800

    add inner product distance measurement function and test

---
+++ include/distance/inner_product.h
@@ -0,0 +1,42 @@
+// Copyright (c) 2024 by dingning
+//
+// file  : inner_product.h
+// since : 2024-07-17
+// desc  : 
+#ifndef VDBMS_DISTANCE_EUCLIDEAN_DISTANCE_H_
+#define VDBMS_DISTANCE_EUCLIDEAN_DISTANCE_H_
+
+#include "basic_distance.h"
+
+namespace tiny_v_dbms {
+
+template<typename DATA_TYPE>
+class InnerProduct : public BasicDistance<DATA_TYPE> {
+
+public:
+
+    InnerProduct() {
+        this->SetCalFunction(InnerProductFunction);
+    }
+
+    /**
+    * @brief The function to calculate the inner product distance between two vectors. 
+    * @param first_vector first vector.
+    * @param second_vector second vector.
+    */
+    static DATA_TYPE InnerProductFunction(BASE_VECTOR & first_vector, BASE_VECTOR & second_vector) {
+
+        assert(first_vector.GetLength() == second_vector.GetLength());
+
+        DATA_TYPE result = 0;
+        for (DATA_TYPE i = 0; i < first_vector.GetLength(); i++) {
+            result += (first_vector.Get(i) * second_vector.Get(i));
+        }
+    
+        return result;
+    }
+    
+};
+
+}
+#endif // VDBMS_DISTANCE_EUCLIDEAN_DISTANCE_H_
