3482c9c6db87029f369ee4eb4246a6e9110f4860
Ding Ning <dingning@whu.edu.cn>
2024-07-17 15:58:44 +0800

    add a basic distance calculation class and test

---
+++ include/config.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2024 by dingning
+//
+// file  : config.h
+// since : 2024-07-16
+// desc  : Basic config of this project, contains the default vector type...
+
+#ifndef VDBMS_CONFIG_H_
+#define VDBMS_CONFIG_H_
+
+#include "meta/vector.h"
+
+#define BASE_VECTOR Vector<int, double>
+
+#endif // VDBMS_CONFIG_H_
