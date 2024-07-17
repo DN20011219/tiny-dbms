47f8a6b62327bd5881a98117e14b7df0422a8d3b
dingning14 <dingning14@meituan.com>
2024-07-17 20:09:36 +0800

    add storage part

---
+++ include/index/basic_index.h
@@ -0,0 +1,20 @@
+// Copyright (c) 2024 by dingning
+//
+// file  : basic_index.h
+// since : 2024-07-17
+// desc  : Base index, which defines the action and interface about an index. Any index must offer the interface defined here.
+
+#ifndef VDBMS_BASIC_INDEX_
+#define VDBMS_BASIC_INDEX_
+
+#include "../config.h" 
+
+namespace tiny_v_dbms {
+
+class BasicIndex {
+
+};
+
+}
+
+#endif // VDBMS_BASIC_INDEX_
