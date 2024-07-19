cf581d23686d4155fef1285a5a53f3e3e1997446
dingning14 <dingning14@meituan.com>
2024-07-19 13:55:30 +0800

    update bench, add install function

--- include/storage/file_management.h
+++ include/storage/file_management.h
@@ -10,6 +10,10 @@
 #include <string>
 #include <fstream>
 
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+
 #include "../config.h" 
 
 namespace tiny_v_dbms {
@@ -33,7 +37,7 @@ public:
         file.close();
     }
 
-    void WriteFile(string& file_dir, char* memory_pointer, default_length_size data_size) {
+    void WriteFile(string& file_dir, const char* memory_pointer, default_length_size data_size) {
         std::ofstream file(file_dir, ios::binary);
         if (!file) {
             throw std::runtime_error("Failed to open file for writing");
@@ -42,6 +46,54 @@ public:
         file.close();
     }
 
+    std::ifstream OpenOrCreateFile(const string& file_path)
+    {
+        std::ifstream file_write;
+
+        file_write.open(file_path, ios::binary);
+        if (!file_write || !file_write.is_open())
+        {
+            std::ofstream fout(file_path);
+            fout.close();
+            file_write.open(file_path, ios::binary);
+        }
+        
+        if (!file_write || !file_write.is_open())
+            throw std::runtime_error("Failed to create file:" + file_path);
+
+        return file_write;
+    }
+
+    void OpencvDirAndMkdir(const string folder_path)
+    {
+        DIR * mydir =NULL;
+        mydir=opendir(folder_path.c_str());
+        if(mydir==NULL)
+        {
+            mkdir(folder_path.c_str(), 0755);
+        }
+        else
+        {
+            closedir(mydir);
+        }
+    }
+
+    void IsEmptyDir(std::string folderPath)
+    {
+        DIR *dir;
+        struct dirent *ent;
+        if ((dir = opendir(folderPath.c_str())) != NULL) 
+        {
+            while ((ent = readdir(dir)) != NULL) 
+            {
+                std::string filename(ent->d_name);
+                if (filename != "." && filename != "..")
+                    throw std::runtime_error("Install path not empty!");
+            }
+            closedir(dir);
+        }
+    }
+    
 private:
     
 };
