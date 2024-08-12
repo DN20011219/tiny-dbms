871a3492b1e11124211e495dfc07c3f4dff4559f
Ding Ning <dingning@whu.edu.cn>
2024-08-12 19:19:29 +0800

    update server and client, using msg queue to communicate

--- include/config.h
+++ include/config.h
@@ -67,6 +67,24 @@ namespace tiny_v_dbms {
     enum column_type {NUMBER, VECTOR, VCHAR};
     enum column_length {NUMBER_LENGTH = 4, VCHAR_LENGTH = 50};
     enum column_index_type {NONE, FLAT};
+
+    // config about client and server
+    #define CONNECTOR_MESSAGE_KEY ftok("tvdbms_connect", 7) 
+    #define CONNECTOR_MESSAGE_ID 40286          // this id is used to confirm the msg queue used by connecter and clients
+    #define WORKER_MESSAGE_ID 40287             // this id is used to confirm the msg queue used by workers and clients
+    #define CONNECTOR_MSG_TYPE_SEND    100      // this type is used when server send msg to client
+    #define CONNECTOR_MSG_TYPE_RECV    200      // this type is used when client send msg to server
+
+    #define MSG_DATA_LENGTH 128                 // config about msg used by connecter
+    #define IP_LENGTH 15
+    #define PORT_LENGTH 4
+    #define IDENTITY_LENGTH 4
+    #define CONNECT_DB_NAME_LENGTH_LENGTH 4
+    #define CONNECT_DB_NAME_LENGTH  100
+
+    #define SQL_MSG_DATA_LENGTH 200             // config about msg used by worker, here limit the max length is 200
+    #define SQL_LENGTH_LENGTH 4
+
 }
 
 #endif // VDBMS_CONFIG_H_
--- include/memory/replacer/block_base_replacer.h
+++ include/memory/replacer/block_base_replacer.h
@@ -74,7 +74,7 @@ public:
     
     bool LoadTableData()
     {
-
+        return true;
     }
 
     // release one block
